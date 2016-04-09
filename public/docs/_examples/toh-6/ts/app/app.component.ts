a8f24799d181fce51735acba853273d9231b2485
Torgeir Helgevold <torgeirh@hotmail.com>
2016-04-09 00:18:37 -0400

    docs:(TOH chapter on Http) New Tour of Hero Chapter on Http
    tweaks based on feedback
    
    text tweaks
    
    grammar
    
    grammar

---
+++ public/docs/_examples/toh-6/ts/app/app.component.ts
@@ -0,0 +1,67 @@
+// #docplaster
+// #docregion
+import { Component } from 'angular2/core';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+
+import { HeroService } from './hero.service';
+import { DashboardComponent } from './dashboard.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+
+import { provide } from 'angular2/core';
+
+//#docregion http
+//Required import for Http in general
+import { HTTP_PROVIDERS } from 'angular2/http';
+
+//These imports are only required for the in-memory web api 
+import { InMemoryBackendService, SEED_DATA } from 'a2-in-memory-web-api/core';
+import { XHRBackend } from 'angular2/http';
+import { HeroData } from './hero-data';
+
+@Component({
+  selector: 'my-app',
+
+  template: `
+    <h1>{{title}}</h1>
+    <nav>
+      <a [routerLink]="['Dashboard']">Dashboard</a>
+      <a [routerLink]="['Heroes']">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['app/app.component.css'],
+  directives: [ROUTER_DIRECTIVES],
+  providers: [
+    ROUTER_PROVIDERS, 
+    HeroService,
+    
+    HTTP_PROVIDERS, //Required registration of HTTP_PROVIDERS 
+    
+    // Only required for the in-memory web api 
+    provide(XHRBackend, { useClass: InMemoryBackendService }), // in-mem server
+    provide(SEED_DATA,  { useClass: HeroData }) // in-mem server data
+  ]
+})
+//#enddocregion http
+@RouteConfig([
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+  },
+  {
+    path: '/detail/:id',
+    name: 'HeroDetail',
+    component: HeroDetailComponent
+  },
+  {
+    path: '/heroes',
+    name: 'Heroes',
+    component: HeroesComponent
+  }
+])
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
