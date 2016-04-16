44c0e5be16537a7aae867f53163697b4155f0153
Torgeir Helgevold <torgeirh@hotmail.com>
2016-04-16 11:11:37 -0400

    docs:(cookbook showing how to create a TreeView)
    working on getting tree view rendering
    
    wiring up tree
    
    more features
    
    styles
    
    added add/cancel
    
    styles
    
    http
    
    fixed styling
    
    added error handling
    
    docs
    
    clean-up
    
    starting article
    
    img
    
    text
    
    updated
    
    e2e
    
    text
    
    text
    
    text
    
    renamed rating
    
    fixed test
    
    typo

---
+++ public/docs/_examples/cb-tree-view/ts/app/main.ts
@@ -0,0 +1,9 @@
+// #docregion
+import {bootstrap}      from 'angular2/platform/browser';
+import {AppComponent}   from './app.component';
+import {HTTP_PROVIDERS} from 'angular2/http';
+
+import 'rxjs/Rx';
+
+bootstrap(AppComponent, [HTTP_PROVIDERS])
+  .catch((err:any) => console.error(err));
